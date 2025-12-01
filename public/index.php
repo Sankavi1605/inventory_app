bdd2a1bd80afe624f09d2e3181f3357f39a1c7dc
Sankavi1605 <155472997+Sankavi1605@users.noreply.github.com>
2025-12-02 00:39:36 +0530

    Debug public index

--- public/index.php
+++ public/index.php
@@ -1,4 +1,28 @@
 <?php
-  require_once '../app/bootloader.php';
-  $init = new Core(); 
+echo "<h1>Public Folder is Reachable!</h1>";
+echo "If you see this, the server routing is perfect.<br>";
+
+// Now let's try to load the bootloader safely
+echo "Attempting to load bootloader...<br>";
+
+if (file_exists('../app/bootloader.php')) {
+    require_once '../app/bootloader.php';
+    echo "Bootloader loaded successfully!<br>";
+} else {
+    die("CRITICAL: Can't find ../app/bootloader.php");
+}
+
+// Check for the Core class
+echo "Checking for Core library...<br>";
+if (class_exists('Core')) {
+    echo "Class 'Core' Found! Starting app...<br>";
+    // $init = new Core(); // We keep this OFF for now
+} else {
+    echo "<span style='color:red'>Error: Class 'Core' NOT found. Autoloader is failing.</span><br>";
+    
+    // Debug: List what IS in libraries
+    echo "<br><b>Debug: Listing app/libraries folder:</b><br>";
+    $libs = scandir('../app/libraries');
+    print_r($libs);
+}
 ?>
